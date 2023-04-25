661edfdc3e4dc3a56027ae50de5242c11a5adce4
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-25 22:09:45 +0800

    proj: setup.py, installed using pip, imported and called

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tabox',
+    version='0.1.0',
+    description='tabox - some technical analysis tools',
+    url='https://github.com/quantumsnowball/tabox',
+    author='Quantum Snowball',
+    author_email='quantum.snowball@gmail.com',
+    license='MIT',
+    packages=['tabox'],
+)
