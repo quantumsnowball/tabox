3b753561e8b151981d4292aca194e82317b72f03
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-28 20:01:27 +0800

    feat: price percentile apply to both Series and DataFrame

--- tabox/momentum.py
+++ tabox/momentum.py
@@ -1,5 +1,16 @@
-from pandas import Series
+from typing import TypeVar
+
+from pandas import DataFrame, Series
+
+T_Pandas = TypeVar('T_Pandas', Series, DataFrame)
 
 
-def price_percentile(win: Series) -> Series:
-    return Series(win.rank(pct=True))
+def price_percentile(win: T_Pandas) -> T_Pandas:
+    if isinstance(win, Series):
+        sr = Series(win.rank(pct=True))
+        return sr
+    elif isinstance(win, DataFrame):
+        df = DataFrame(win.rank(axis=0, pct=True))
+        return df
+    else:
+        raise TypeError('Only support Series or DataFrame')
